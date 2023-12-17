6d220a1274da32685770bd83c5a556e3fc80b233
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-12-17 16:54:34 +0530

    style: dynamic sidebar

--- src/app/(browse)/layout.tsx
+++ src/app/(browse)/layout.tsx
@@ -1,10 +1,15 @@
-import Navbar from "@/components/Navbar";
+import Container from '@/components/Container';
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
 
 const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
-      <div className="flex h-full pt-20 ">{children}</div>
+      <div className='flex h-full pt-20 '>
+        <Sidebar />
+        <Container>{children}</Container>
+      </div>
     </>
   );
 };
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import Toggle from './Toggle';
+import Wrapper from './Wrapper';
+
+const Sidebar = () => {
+  return (
+    <Wrapper>
+      <Toggle />
+    </Wrapper>
+  );
+};
+export default Sidebar;
